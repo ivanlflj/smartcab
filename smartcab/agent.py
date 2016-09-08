4f03b6f9ffdb08b9134effc51d7f8b13616b1e4b
Ivan Landim <ivanlflj@gmail.com>
2016-09-08 23:35:23 +0200

    Implement Q function

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -12,6 +12,8 @@ class LearningAgent(Agent):
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
         self.possible_actions = [None, 'forward', 'left', 'right']
+        self.q = {}
+        self.alpha = 0.5
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
@@ -26,13 +28,24 @@ class LearningAgent(Agent):
         # TODO: Update state
         self.state = [self.next_waypoint, inputs]
         
+        max = -float('Inf')
+        best_action = 'forward'
+        for action in self.possible_actions:
+            try:
+                if self.q[str([self.state,action])] >= max:
+                    max = self.q[str([self.state,action])]
+                    best_action = action
+            except:
+                self.q[str([self.state,action])] = 0.
+
         # TODO: Select action according to your policy
-        action = random.choice(self.possible_actions)
+        action = best_action
 
         # Execute action and get reward
         reward = self.env.act(self, action)
 
         # TODO: Learn policy based on state, action, reward
+        self.q[str([self.state,action])] = self.q[str([self.state,action])] + self.alpha * reward
 
         print "LearningAgent.update(): deadline = {}, inputs = {}, action = {}, reward = {}".format(deadline, inputs, action, reward)  # [debug]
 
@@ -43,11 +56,11 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
-    sim = Simulator(e, update_delay=0.5, display=True)  # create simulator (uses pygame when display=True, if available)
+    sim = Simulator(e, update_delay=0.5, display=False)  # create simulator (uses pygame when display=True, if available)
     # NOTE: To speed up simulation, reduce update_delay and/or set display=False
 
     sim.run(n_trials=100)  # run for a specified number of trials
